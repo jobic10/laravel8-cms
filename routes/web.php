11883c33d5caa359eec4c0f6e61a3b2214f99c41
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-01-05 16:07:13 +0100

    Grouped Routes

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+
+
+Route::group(['middleware' => ['auth:sanctum','verified']], function(){
+    Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
