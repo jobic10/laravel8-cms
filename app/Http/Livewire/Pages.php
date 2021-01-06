7354cf378b377e1ee95443e5d3e155c921a7b29a
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-01-06 04:41:23 +0100

    Styling

--- app/Http/Livewire/Pages.php
+++ app/Http/Livewire/Pages.php
@@ -2,10 +2,32 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Page;
 use Livewire\Component;
 
 class Pages extends Component
 {
+
+    public $slug;
+    public $title;
+    public $content;
+    public  $modalFormVisible = false;
+
+    /**
+     * Shows the form modal
+     * of the create function.
+     *
+     * @return void
+     */
+    public function createShowModal(){
+        $this->modalFormVisible = true;
+    }
+
+    /**
+     * The livewire render function.
+     *
+     * @return void
+     */
     public function render()
     {
         return view('livewire.pages');
